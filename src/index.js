1f1f6666431d2e3039cfb0d22ad6bb974c8a05b1
Peter Treitler <peter.treitler@gmail.com>
2020-03-30 20:17:22 +0200

    Implement move history

--- src/index.js
+++ src/index.js
@@ -51,15 +51,28 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
-    
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const description = move ?
+      'Go to move #' + move :
+      'Go to game start';
+
+      return(
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{description}</button>
+        </li>
+      );
+    });
+
     let status;
 
     if (winner) {
@@ -79,14 +92,14 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -98,6 +111,14 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{squares: squares}]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+  
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext : (step % 2) === 0,
     });
   }
 }
